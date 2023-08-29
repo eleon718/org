1b878268950b597731041627ceb657253d6ed009
Elliot <elliot.leon718@gmail.com>
2023-08-29 14:47:44 -0600

    Agregando props a nuestro componente campo de texto para modificar las etiquetas label del componente

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,11 @@
 import "./CampoTexto.css";
 
-const CampoTexto = () => {
+const CampoTexto = (props) => {
+  const placeholderModificado = `${props.placeholder}...`;
   return (
     <div className="campo-texto">
-      <label>Nombre</label>
-      <input placeholder="Ingresar nombre" />
+      <label>{props.titulo.toUpperCase()}</label>
+      <input placeholder={placeholderModificado} />
     </div>
   );
 };
--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -6,9 +6,9 @@ const Formulario = () => {
     <section className="formulario">
       <form>
         <h2>Rellena el formulario para crear el colaborador</h2>
-        <CampoTexto />
-        <CampoTexto />
-        <CampoTexto />
+        <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" />
+        <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" />
+        <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto" />
       </form>
     </section>
   );
