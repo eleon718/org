4107f9074d7b3c015c0324002c7f91f1b69f69b9
Elliot <elliot.leon718@gmail.com>
2023-08-29 13:04:10 -0600

    Agregando el componente Campo de texto y usandolo dentro del componente Formulario

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import "./CampoTexto.css";
+
+const CampoTexto = () => {
+  return (
+    <div className="campo-texto">
+      <label>Nombre</label>
+      <input placeholder="Ingresar nombre" />
+    </div>
+  );
+};
+
+export default CampoTexto;
