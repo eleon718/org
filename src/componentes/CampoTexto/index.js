35aae1f5952397801158e835d1c1cf87da36d521
Elliot <elliot.leon718@gmail.com>
2023-08-31 12:56:00 -0600

    Agregando hooks a los elementos del formulario

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -2,10 +2,19 @@ import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
   const placeholderModificado = `${props.placeholder}...`;
+
+  const manejarCambio = (e) => {
+    props.actualizarValor(e.target.value);
+  };
   return (
     <div className="campo-texto">
       <label>{props.titulo.toUpperCase()}</label>
-      <input placeholder={placeholderModificado} required={props.required} />
+      <input
+        placeholder={placeholderModificado}
+        required={props.required}
+        value={props.valor}
+        onChange={manejarCambio}
+      />
     </div>
   );
 };
--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto";
 import ListaOpciones from "../ListaOpciones";
 import Boton from "../Boton";
 
 const Formulario = () => {
+  const [nombre, actualizarNombre] = useState("");
+  const [puesto, actualizarPuesto] = useState("");
+  const [foto, actualizarFoto] = useState("");
+  const [equipo, actualizarEquipo] = useState("");
+
   const manejarEnvio = (e) => {
     e.preventDefault();
+    let datosAEnviar = {
+      nombre,
+      puesto,
+      foto,
+      equipo,
+    };
   };
 
   return (
@@ -16,14 +28,24 @@ const Formulario = () => {
           titulo="Nombre"
           placeholder="Ingresar nombre"
           required={true}
+          valor={nombre}
+          actualizarValor={actualizarNombre}
         />
-        <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" required />
+        <CampoTexto
+          titulo="Puesto"
+          placeholder="Ingresar puesto"
+          required
+          valor={puesto}
+          actualizarValor={actualizarPuesto}
+        />
         <CampoTexto
           titulo="Foto"
           placeholder="Ingresar enlace de foto"
           required
+          valor={foto}
+          actualizarValor={actualizarFoto}
         />
-        <ListaOpciones />
+        <ListaOpciones valor={equipo} actualizarEquipo={actualizarEquipo} />
         <Boton>Crear</Boton>
       </form>
     </section>
