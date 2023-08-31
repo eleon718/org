6b5d988af80f27697efdfc2ac94fa142fc2e4309
Elliot <elliot.leon718@gmail.com>
2023-08-30 23:09:07 -0600

    Cambiando el estado del formulario para ocultarlo y mostrarlo presionando el boton con la imagen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./componentes/Header/Header.js";
 import Formulario from "./componentes/Formulario/Formulario.js";
+import MiOrg from "./componentes/MiOrg";
 
 function App() {
+  const [mostrarFormulario, actualizarMostrar] = useState(true);
+
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario);
+  };
+
   return (
     <div>
       <Header />
-      <Formulario />
+      {/*       {mostrarFormulario === true ? <Formulario /> : <></>}
+       */}
+      {mostrarFormulario && <Formulario />}
+      <MiOrg cambiarMostrar={cambiarMostrar} />
     </div>
   );
 }
